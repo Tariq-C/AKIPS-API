dd978610f37f868e17615cfafa5d3c4d501e916f
Tariq Chatur <tariqchatur@gmail.com>
2025-04-21 10:58:12 -0700

    Create NMDB.pm

---
+++ API/AKIPS/NMDB.pm
@@ -0,0 +1,38 @@
+#!/opt/bcgov/bin/perl
+# Developer: Tariq Chatur
+# Description:
+#  This module contains the NM-DB related functions of AKIPS API Wrapper
+#
+# Last Modified: March 20th 2025
+package API::AKIPS::NMDB;
+
+   use API::AKIPS::Rest;
+
+   # The Constructor of this module
+   sub new {
+
+      my $class = shift;
+      my $self = {
+            rest     =>  API::AKIPS::Rest->new()
+      };
+      bless $self,$class;
+      return $self;
+   }
+
+   # Description:
+   #  This function is a general call to utilize the command console on AKIPS
+   # Param: String - The Query to be run by the command console
+   # Return String - The output of the query in csv a format
+   sub nm_db {
+      my $self = shift;
+      my $query = shift;
+      my $username = $self->{rest}->get_rw_user();
+      my $password = $self->{rest}->get_password();
+      my $full_url = $self->{base_url}.
+                     "username=".$username.
+                     ";password=".$password.
+                     ";cmds=".$query;
+      return $self->{rest}->request($full_url, "nm_db $query");
+   }
+
+1;
